a11ef1b2bf84474ac20c6a9aecac9f183909e7f7
Kingshuk Deb <74722462+Kingshuk-Deb@users.noreply.github.com>
2021-07-18 00:48:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <Body />
+  );
+}
+
+export default App;
